2011dffee2417cf5bf959a8305673734b62efd19
vitaliiPsl <vitalii.psl.main@gmail.com>
2021-10-02 23:39:23 +0300

    Main logic

---
+++ sketchr/main.py
@@ -0,0 +1,121 @@
+from logging import error
+import re
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from flask.json import jsonify
+
+from werkzeug.exceptions  import abort
+
+from sketchr.auth import login_required
+
+from sketchr.db import get_db
+
+bp = Blueprint('main', __name__)
+
+
+@bp.route('/')
+def index():
+    id = request.args.get("id")
+
+    if id is not None:
+        print("Not null")
+        db = get_db()
+        record = db.execute(
+            'SELECT id, author_id '
+            'FROM picture where id = ?',
+            (id)
+        ).fetchone()
+
+        if record is None:
+            error = "Incorrect id request"
+            flash(error)
+            return render_template('index.html')
+
+        return render_template('index.html', record=record)
+
+    return render_template('index.html')
+
+
+@bp.route('/gallery')
+def gallery():
+
+    return render_template('gallery.html')
+
+
+@bp.route('/get_gallery_records')
+def get_gallery_records():
+    
+    db = get_db()
+    pictures = db.execute(
+        'SELECT p.id, title, body, created, size, author_id, username '
+        'FROM picture as p JOIN user as u ON p.author_id = u.id '
+        'ORDER BY title'
+    ).fetchall()
+
+    records = {'pictures': []}
+
+    for picture in pictures:
+        record = {}
+        record['id'] = picture['id']
+        record['username'] = picture['username']
+        record['created'] = picture['created']
+        record['title'] = picture['title']
+        record['size'] = picture['size']
+        record['body'] = picture['body']
+
+        records['pictures'].append(record)   
+    
+    return records
+
+
+@bp.route('/get_record', methods=['POST'])
+def get_record():
+
+    id = request.json['recordId']
+
+    if id is None:
+        return {'result': "Wrong id"}
+
+    db = get_db()
+    record = db.execute(
+        'SELECT title, body, size '
+        'FROM picture '
+        'WHERE id = ?',
+        (id)
+    ).fetchone()
+
+    if record is None:
+        return {'result': "Wrong id"}
+
+    return {
+        'title': record['title'],
+        'size': record['size'],
+        'body': record['body']
+    }
+
+
+@bp.route('/save', methods=['POST'])
+def save():
+    picture_data = request.json
+
+    error = None
+    title = picture_data['title']
+    size = int(picture_data['size'])
+    body = str(picture_data['body'])
+    author_id = session.get('user_id')
+    
+    if error is None:
+        db = get_db()
+        db.execute(
+            'INSERT INTO picture (author_id, title, size, body) '
+            'VALUES (?, ?, ?, ?)',
+            (author_id, title, size, body)
+        )
+        db.commit()
+        print("Success")
+        return {'result' : "Success"}
+
+    flash(error)
+    
+    return render_template("index.html")
